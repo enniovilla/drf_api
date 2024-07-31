506eba1f0a25090da77da6f5f93b75c50f97cd82
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-07-31 17:15:38 +0000

    Add post to admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'owner', 'created_at', 'updated_at', 'image_filter')
+    search_fields = ('title', 'content')
+    list_filter = ('image_filter', 'created_at', 'owner')
+    ordering = ('-created_at',)
+    fields = ('title', 'content', 'image', 'image_filter', 'owner')
+
+admin.site.register(Post, PostAdmin)

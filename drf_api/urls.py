622b97db9d7c3f3e39fd9b4e1027cf98fe0539d5
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-07-31 12:05:00 +0000

    Undo CurrentUserContext fix

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route, CustomUserDetailsView
+from .views import root_route
 
 urlpatterns = [
     path('', root_route),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/user/', CustomUserDetailsView.as_view(), name='custom_user_details'),
     path(
         'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
     ),

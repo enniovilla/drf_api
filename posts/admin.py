16e15af1286c32ae4c35622e9c87387a7d7279d0
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-07-31 17:28:48 +0000

    Undo post admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import Post
 
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'owner', 'created_at', 'updated_at', 'image_filter')
-    search_fields = ('title', 'content')
-    list_filter = ('image_filter', 'created_at', 'owner')
-    ordering = ('-created_at',)
-    fields = ('title', 'content', 'image', 'image_filter', 'owner')
-
-admin.site.register(Post, PostAdmin)
+# Register your models here.
